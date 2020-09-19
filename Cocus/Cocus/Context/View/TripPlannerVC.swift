6c2a30edc71deefcfba8cfda39132808944c68a3
emir daliri <amir.dalirii69@gmail.com>
2020-09-19 20:00:33 +0300

    add basic UI, extensions class for UI

--- Cocus/Cocus/Context/View/TripPlannerVC.swift
+++ Cocus/Cocus/Context/View/TripPlannerVC.swift
@@ -7,15 +7,30 @@
 //
 
 import UIKit
+import MapKit
 
 class TripPlannerVC: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet private weak var priceLabel: UILabel!
+    @IBOutlet private weak var fromTextfield: UITextField!
+    @IBOutlet private weak var toTextField: UITextField!
+    @IBOutlet private weak var mapView: MKMapView!
+    @IBOutlet private weak var showWayButton: UIButton!
+    
     // MARK: - Lifecycle Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // I'm Here...
+        setupView()
     }
-
+    
+    func setupView() {
+        fromTextfield.addDoneButtonOnKeyboard()
+        toTextField.addDoneButtonOnKeyboard()
+        mapView.cornerRadius = 8
+        showWayButton.cornerRadius = 8
+    }
 }
---
+++ Cocus/Cocus/Misc/UIExtensions.swift
@@ -0,0 +1,41 @@
+//
+//  UIExtensions.swift
+//  Cocus
+//
+//  Created by Amir Daliri on 19.09.2020.
+//  Copyright © 2020 Cocus. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - UIView Method
+extension UIView {
+    var cornerRadius: CGFloat {
+      get {
+        return layer.cornerRadius
+      }
+      set {
+        layer.cornerRadius = newValue
+      }
+    }
+}
+
+// MARK: - UITextField Method
+extension UITextField {
+    func addDoneButtonOnKeyboard() {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+        //        done.tintColor = UIColor.gray
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        self.inputAccessoryView = doneToolbar
+    }
+    
+    @objc func doneButtonAction() {
+        self.resignFirstResponder()
+    }
+}
